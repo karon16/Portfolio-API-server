5b88a77cd9a715949a1090ec937c28ffe7ce5687
christopher buhendwa <christopherbuhendwa2000@gmail.com>
2021-09-29 11:32:22 +0100

    feat : conncect database to clevercloud

--- src/config/db.connection.js
+++ src/config/db.connection.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-undef */
-import mysql from "mysql2";
-import dotenv from "dotenv";
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: 'b5evv9k9bu7ogk79mdek-mysql.services.clever-cloud.com',
+  user: 'uskxya4l28bp2dvz',
   password: process.env.SQLPASSWORD,
-  database: "projet_portfolio",
+  database: 'b5evv9k9bu7ogk79mdek',
 });
 
 connection.connect((err) => {
   if (err) {
     console.log(err);
-    console.error("error connecting on port 4000");
+    console.error('error connecting on port 4000');
     return;
   }
 });
